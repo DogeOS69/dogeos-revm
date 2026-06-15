13287f5a2152731f5753f192d73f98cd3ec75871
Akase Haruka <lightsing@users.noreply.github.com>
2026-06-15 15:48:36 +0800

    feat(precompile): enable ripemd160 (#4)
    * feat: enable ripemd160 with size limit
    
    * add test

--- src/precompile/hash.rs
+++ src/precompile/hash.rs
@@ -19,7 +19,7 @@ pub mod sha256 {
 
 pub mod ripemd160 {
     use super::*;
-    use revm::precompile::{u64_to_address, Precompile, PrecompileId};
+    use revm::precompile::{u64_to_address, Precompile, PrecompileError, PrecompileId};
 
     /// The RIPEMD160 precompile address.
     pub const ADDRESS: Address = u64_to_address(3);
@@ -29,4 +29,18 @@ pub mod ripemd160 {
     /// This precompile is not implemented and will return `PrecompileError::Other("Precompile not
     /// implemented".into())`.
     pub const SHANGHAI: Precompile = precompile_not_implemented(PrecompileId::Ripemd160, ADDRESS);
+
+    /// The maximum length of the input for the RIPEMD160 precompile in the GALDOGEOS hardfork.
+    pub const GALDOGEOS_LEN_LIMIT: usize = 32;
+
+    /// The GALDOGEOS RIPEMD160 is enabled.
+    pub const GALDOGEOS: Precompile =
+        Precompile::new(PrecompileId::Ripemd160, ADDRESS, |input, gas_limit| {
+            if input.len() > GALDOGEOS_LEN_LIMIT {
+                return Err(PrecompileError::Other(
+                    "Ripemd160InputOverflow: ripemd160 input overflow".into(),
+                ));
+            }
+            hash::ripemd160_run(input, gas_limit)
+        });
 }
